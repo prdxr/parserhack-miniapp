997a4d8f59a4e2b112fb182a4a0e595a7ef9ed6b
neverelz <belyavsteva04@mail.ru>
2024-10-19 16:39:46 +0000

    from server commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -34,7 +34,7 @@ DEBUG = bool(int(os.environ.get('DEBUG')))
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:83',
-	'http://10.90.192.15:83'
+	'http://88.218.67.139:83'
 ]
 # Application definition
 
